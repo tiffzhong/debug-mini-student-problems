3587e036d002eb3b35e390dccce528233067f08b
Tiffany Zhong <tiffanyzhong@tiffz.local>
2018-12-18 14:39:31 -0700

    used body parser

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ See README.md in master branch.
 
 ---
 
+problem #5
+needed app.use(bodyParser.json()); in index.js
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
+app.use(bodyParser.json());
 
-app.post('/api/data', (req, res) => {
+app.post("/api/data", (req, res) => {
   res.json({ message: `The value you sent was ${req.body.value}` });
 });
 
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
 app.listen(SERVER_PORT, () => {
-  console.log('Server listening on port ' + SERVER_PORT);
+  console.log("Server listening on port " + SERVER_PORT);
 });
